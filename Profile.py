1a55b753486749fde82924bdd0fc83258464c107
Léo Peltier <contact@leo-peltier.fr>
2014-02-01 23:11:22 +0100

    Generate BS.

--- Profile.py
+++ Profile.py
@@ -1,4 +1,7 @@
 from sklearn.externals import joblib
+from wastburg.models import DjuDay
+from licence4.settings import KWH_TO_EUROS, DJU_TO_KWH
+import numpy as np
 
 class Profile:
     cost = None # [doy, temp, lot.surface, dju] -> cost
@@ -10,7 +13,34 @@ class Profile:
         self.temp = joblib.load("data/temp/dump")
         self.dju  = joblib.load("data/dju/dump")
 
-    def predict_cost(self, doys, surface):
+
+    def get_goal(self, surface):
+        """Return the spending goal for the current year."""
+        return sum(self.get_daily_goals(surface))
+
+
+    def get_bullshit_heat_spendings(self, surface, mu, sigma):
+        heat = self.get_daily_goals(surface)
+        noise = np.random.normal(mu, sigma, len(heat))
+        return map((lambda x,y: x+y), noise, heat)
+
+
+    def get_daily_goals(self, surface):
+        """
+        Return a list of amounts, one per day, of average spending.
+        @return float[] in euro
+        """
+        iterator = DjuDay.objects.filter(day__year=2013).values("average")
+        return [
+            x["average"] * DJU_TO_KWH * KWH_TO_EUROS * surface / 1000 for x in iterator
+        ]
+
+
+    def predict_costs(self, doys, surface):
+        """
+        Predict the spendings for 365 days.
+        @return float[] in euro
+        """
         djus = self.dju.predict([[x] for x in doys])
         temps = self.temp.predict(zip(doys, djus))
         surfaces = [surface for x in doys]
--- wastburg/management/commands/plot.py
+++ wastburg/management/commands/plot.py
@@ -1,14 +1,23 @@
+from Profile import Profile
 from django.core.management.base import BaseCommand
 from matplotlib import pyplot as plt
-from Profile import Profile
+from wastburg.models import Lot
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         p = Profile()
+        lot = Lot.objects.get(name="B123")
+        goal = p.get_goal(lot.surface)
+        heat = p.get_bullshit_heat_spendings(lot.surface, .3, .3)
 
         doys = range(365)
-        plt.hold(True)
-        plt.plot(doys, p.predict_cost(doys, surface=20), c='r')
+        goal_daily = p.get_daily_goals(lot.surface)
+        diff = map((lambda x,y: goal + x-y), heat, goal_daily)
+
+        plt.plot(doys[:150], diff[:150])
+        plt.plot(doys[150:], diff[150:], '--')
+        plt.axhline(goal, label="goal", c="b")
         plt.xlabel("day")
         plt.ylabel("cost")
+        plt.legend()
         plt.show()
